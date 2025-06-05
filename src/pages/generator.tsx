24f115177e59b6bb41c781c9759ed138db89480b
Kailouis <itsmedodhay@gmail.com>
2025-06-05 21:03:02 +0700

    chore: clean unused tokens

--- src/pages/generator.tsx
+++ src/pages/generator.tsx
@@ -120,9 +120,6 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         
           {/* Preload font */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
 
       <Navbar />
